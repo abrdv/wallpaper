719c76df4f90f06dfc4d8327da6a9cf9fbd50f3a
abrdv <abrdv@mail.ru>
2024-05-29 14:34:02 +0200

    first

---
+++ PWPCH.dpr
@@ -0,0 +1,16 @@
+// JCL_DEBUG_EXPERT_INSERTJDBG OFF
+// JCL_DEBUG_EXPERT_DELETEMAPFILE OFF
+program PWPCH;
+
+uses
+  Vcl.Forms,
+  UWPCH in 'UWPCH.pas' {FWallPaper};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFWallPaper, FWallPaper);
+  Application.Run;
+end.
